36e60fbf1bf3d333fee1112099e0a92605fa5271
sam38124 <sam38124@gmail.com>
2020-10-18 05:09:13 +0800

    4.2

--- jzutil/src/main/java/com/orango/electronic/jzutil/serilExtension.kt
+++ jzutil/src/main/java/com/orango/electronic/jzutil/serilExtension.kt
@@ -42,7 +42,7 @@ inline fun <reified T> String.getObject(rout: String = "file"): T? {
     }
 }
 //刪除序列化物件
- fun String.delete(rout: String = "file"): Boolean? {
+ fun String.deleteObject(rout: String = "file"): Boolean? {
     try {
         sqlClass.getControlInstance().item_File.exsql("delete from $rout where name='$this'")
         return true
@@ -70,7 +70,7 @@ inline fun <reified T> String.getObject(rout: String = "file"): T? {
 }
 
 //清空此路徑序列化物件
-fun String.clearObject(rout: String = "file"): Boolean {
+fun String.deleteSerialRout(): Boolean {
     try {
         sqlClass.getControlInstance().item_File.exsql("DROP TABLE $this;\n")
         return true
